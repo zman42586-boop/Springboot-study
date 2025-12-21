5b4241c414221ae3f93b573008246d6d6f23cfac
Zhangjunjie <zman42586@gmail.com>
2025-12-21 19:58:08 +0800

    第一天springboot

---
+++ springboot_day1/src/main/java/com/zhangjunjie/springboot_day1/SpringbootDay1Application.java
@@ -0,0 +1,13 @@
+package com.zhangjunjie.springboot_day1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDay1Application.class, args);
+    }
+
+}
---
+++ springboot_day1/src/test/java/com/zhangjunjie/springboot_day1/SpringbootDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangjunjie.springboot_day1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDay1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
