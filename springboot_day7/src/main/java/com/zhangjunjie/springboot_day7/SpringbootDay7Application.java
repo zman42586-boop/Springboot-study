a58983ebb48b15e637625267fbbceb573acad4da
Zhangjunjie <zman42586@gmail.com>
2025-12-29 19:02:42 +0800

    23-最后 没看mybatis 后面补

---
+++ springboot_day7/src/main/java/com/zhangjunjie/springboot_day7/SpringbootDay7Application.java
@@ -0,0 +1,13 @@
+package com.zhangjunjie.springboot_day7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDay7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDay7Application.class, args);
+    }
+
+}
---
+++ springboot_day7/src/test/java/com/zhangjunjie/springboot_day7/SpringbootDay7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangjunjie.springboot_day7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDay7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
