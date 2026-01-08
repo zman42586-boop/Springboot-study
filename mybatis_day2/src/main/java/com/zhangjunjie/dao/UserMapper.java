abf847f4d6e36ac8b4d24128173370e1479d5ec8
Zhangjunjie <zman42586@gmail.com>
2026-01-08 19:49:44 +0800

    mybatis 6-9

---
+++ mybatis_day2/src/main/java/com/zhangjunjie/dao/UserMapper.java
@@ -0,0 +1,18 @@
+package com.zhangjunjie.dao;
+
+import com.zhangjunjie.pojo.User;
+
+import java.util.List;
+
+public interface UserMapper {
+    //查询
+    List<User> getUserList();
+    //根据ID查询用户
+    User getUserById(int id);
+    //增加用户
+    void addUser(User user);
+    //删除用户
+    void deleteUserById(int id);
+    //修改用户
+    void updateUser(User user);
+}
